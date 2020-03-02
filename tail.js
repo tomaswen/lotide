0bb423c1a61795dfbce3b666b94597c445c6a5f8
Tomas Wen Liang <tomas.wenn@gmail.com>
2020-03-02 19:20:08 +0000

    Returns the tail of an array

--- tail.js
+++ tail.js
@@ -14,7 +14,7 @@ const tail = function(arr) {
 
 
 const result = tail(["Hello", "Tomas", "Wen", "Good morning"]);
-assertEqual(result.length, 3); 
-assertEqual(result[0], "Tomas"); 
-assertEqual(result[1], "Wen"); 
-assertEqual(result[2], "Good morning"); 
+assertEqual(result.length, 3);
+assertEqual(result[0], "Tomas");
+assertEqual(result[1], "Wen");
+assertEqual(result[2], "Good morning");
