ba4cd468b51c3ef307ff6a1a4b4d5fbe542aaf5e
Tomas Wen Liang <tomas.wenn@gmail.com>
2020-03-03 21:09:08 +0000

    changed and added files

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -18,20 +18,3 @@ const assertArraysEqual = function(arr1, arr2){
     console.log ('Your arrays does not match')
   }
 };
-
-const without = function(original, toRemove) {
-  let result = original;
-  let arr = toRemove;
-  for (let i = 0; i < arr.length; i++) {
-    let wordToRemove = arr[i];
-    for (let j = result.length - 1; j >= 0; j--) {
-      if (result[j] === wordToRemove) {
-        result.splice(j,1);
-      }
-    }
-  }
-  return result;
-};
-
-assertArraysEqual(without([1,2,3,4,5,5,6,2],[5,2]),[1,3,4,6]);
-
