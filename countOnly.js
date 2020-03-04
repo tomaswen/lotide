065a5e2c1d8b3afa1c4fdca7ecbfbac5dda9f0a9
Tomas Wen Liang <tomas.wenn@gmail.com>
2020-03-04 16:28:49 +0000

    Function counts and returns an object

---
+++ countOnly.js
@@ -0,0 +1,57 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const count = function(arr1,name){
+  let count = 0;
+  for (let i = 0; i < arr1.length; i++){
+    if (arr1[i] === name){
+      count += 1;
+    }
+  }
+  return count;
+}
+
+const countOnly = function(allItems, itemsToCount) {
+  let result =  {};
+  arr = Object.keys(itemsToCount)
+  for (const names of arr) {
+    let count = 0;
+    for (const items of allItems) {
+      if ( items === names){
+        count += 1;
+      }
+      result[`${names}`] = count;
+      //delete the key if the value of it is 0
+      if (result[`${names}`] === 0 ){
+        delete result[`${names}`];
+      }
+    }
+  }
+  
+  return result
+}
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe",
+  
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+
+
