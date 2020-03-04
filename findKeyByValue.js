5c14d5839cf531e4cf7c38ebf10073a990848659
Tomas Wen Liang <tomas.wenn@gmail.com>
2020-03-04 18:34:08 +0000

    Function returns the key for the value being seached in the object

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(searchObject, searchValue){
+  for (let key in searchObject){
+    if (searchObject[key] === searchValue){
+      return key;
+    }
+  }
+  return undefined;
+};
+
+
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
