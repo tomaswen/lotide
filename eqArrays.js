660c7718ff0acc47607b68cb1ea03b421a3d8e76
Tomas Wen Liang <tomas.wenn@gmail.com>
2020-03-09 14:58:09 +0000

    Dry eqArrays and test

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual')
 
 const eqArrays = function(arr1,arr2) {
   if (arr1.length !== arr2.length) {
@@ -18,4 +12,4 @@ const eqArrays = function(arr1,arr2) {
   return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [2, 2, 3]), true);
+module.exports = eqArrays;
