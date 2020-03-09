90c3356a1d8a22f022653384764215d3e1b8dbed
Tomas Wen Liang <tomas.wenn@gmail.com>
2020-03-09 16:47:13 +0000

    Changes

--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,18 @@
 const head = require('../head')
-const assertEqual = require('../assertEqual')
+const assert = require('chai').assert;
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+  it("returns 5 for [5,6,7]", () => {
+    assert.equal(head([5,6,7]), 5);
+  });
+  it("returns 'hello' for ['hello','lighthouse','labs']", () => {
+    assert.equal(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+});
--- test/middleTest.js
+++ test/middleTest.js
@@ -6,4 +6,5 @@ assertArraysEqual(middle([1,2,3,4,5]),[3]);
 assertArraysEqual(middle([1,2,3,4]),[2]);
 assertArraysEqual(middle([1,2,3,4]),[2,3]);
 assertArraysEqual(middle([1,2,3,4,5,6]),[3,4]);
-assertArraysEqual(middle([1,2,3,4,5,6]),[2]);
+
+
